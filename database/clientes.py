88626cd88884c0be5a9101441d85cb115c2296e1
Miquéias Abilio <miqueias880@gmail.com>
2025-02-01 03:26:54 -0300

    primeiro commit

---
+++ database/clientes.py
@@ -0,0 +1,8 @@
+CLIENTES = [
+    {'id':1, 'nome':'Miquéias','email':'miqueias@gmail.com'},
+    {'id':2, 'nome':'Gislaine','email':'gica@gmail.com'},
+    {'id':3, 'nome':'Vicente','email':'vicente@gmail.com'},
+    {'id':4, 'nome':'Helda','email':'helda@gmail.com'},
+    {'id':5, 'nome':'Marcos','email':'marcos@gmail.com'},
+    {'id':6, 'nome':'Mateus','email':'mateus@gmail.com'},
+]
---
+++ routes/cliente.py
@@ -0,0 +1,50 @@
+from flask import Blueprint, render_template, request
+from database.clientes import CLIENTES
+
+cliente_route = Blueprint('cliente', __name__)
+
+@cliente_route.route('/')
+def lista_cliente():
+    '''listar os clientes'''
+    return render_template('lista_cliente.html', clientes=CLIENTES)
+
+@cliente_route.route('/', methods=['POST'])
+def inserir_cliente():
+    '''inserir os dados do cliente'''
+    data = request.json
+    novo_usuario = {
+        "id": len(CLIENTES) + 1,
+        "nome":data['nome'],
+        "email":data['email']
+    }
+    CLIENTES.append(novo_usuario)
+    return render_template('iten_cliente.html', cliente=novo_usuario)
+
+@cliente_route.route('/new')
+def form_cliente():
+    '''formulario para cadatrar clientes'''
+    return render_template('form_cliente.html')
+
+
+@cliente_route.route('/<int:cliente_id>')
+def detalhe_cliente(cliente_id):
+    '''obter as informações de um cliente'''
+    return render_template('detalhe_cliente.html')
+
+
+@cliente_route.route('/<int:cliente_id>/edit')
+def form_idit_cliente(cliente_id):
+    '''formulario para editar um cliente'''
+
+@cliente_route.route('/<int:cliente_id>/update', methods=['PUT'])
+def atualizar_cliente(cliente_id):
+    '''atualizar os dados do cliente'''
+    pass
+
+@cliente_route.route('/<int:cliente_id>/delete', methods=['DELETE'])
+def deletar_cliente(cliente_id):
+    '''deletar cliente'''
+    global CLIENTES
+    CLIENTES = [ c for c in CLIENTES if c['id'] != cliente_id ]
+    return {'dilited': 'ok'}
+
