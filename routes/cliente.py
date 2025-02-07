8bfdec00fa5a6627a01ca0465b036d60aa33c3e8
Miquéias Abilio <miqueias880@gmail.com>
2025-02-07 05:25:32 -0300

    Editar-update-atualizar

--- routes/cliente.py
+++ routes/cliente.py
@@ -23,23 +23,37 @@ def inserir_cliente():
 @cliente_route.route('/new')
 def form_cliente():
     '''formulario para cadatrar clientes'''
+
     return render_template('form_cliente.html')
 
 
 @cliente_route.route('/<int:cliente_id>')
 def detalhe_cliente(cliente_id):
     '''obter as informações de um cliente'''
-    return render_template('detalhe_cliente.html')
+    cliente = list(filter(lambda c: c['id'] == cliente_id, CLIENTES))[0]
+    return render_template('detalhe_cliente.html',  cliente = cliente)
 
 
 @cliente_route.route('/<int:cliente_id>/edit')
-def form_idit_cliente(cliente_id):
+def form_edit_cliente(cliente_id):
     '''formulario para editar um cliente'''
+    cliente = None
+    for c in CLIENTES:
+        if c['id'] == cliente_id:
+            cliente = c
+    return render_template('form_cliente.html', cliente=cliente)
 
 @cliente_route.route('/<int:cliente_id>/update', methods=['PUT'])
 def atualizar_cliente(cliente_id):
     '''atualizar os dados do cliente'''
-    pass
+    data = request.json
+    cliente_editado = None
+    for c in CLIENTES:
+        if c['id'] == cliente_id:
+            c['nome'] = data['nome']
+            c['email'] = data['email']
+    cliente_editado = c
+    return render_template('iten_cliente.html', cliente = cliente_editado  )
 
 @cliente_route.route('/<int:cliente_id>/delete', methods=['DELETE'])
 def deletar_cliente(cliente_id):
